cf3bcf0cf4034b4cc8aa8277d5bab7002b1f408b
Sunset <nilay.sabnis05@gmail.com>
2024-04-21 17:51:32 +0530

    oops

---
+++ frontend_code/src/back.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+const baseUrl = '/api/contacts';
+
+const getContacts = () =>{
+    return axios.get(baseUrl)
+}
+
+const newContact = (newContact) => {
+    return axios
+        .post(baseUrl, newContact)
+}
+
+const deleteContact = (delId) => {
+    return axios
+        .delete(`${baseUrl}/${delId}`)
+}
+
+export default {
+    getContacts,
+    newContact,
+    deleteContact  
+} 
